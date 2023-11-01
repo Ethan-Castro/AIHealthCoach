54294a6dc4641062702c4d239f36fdc28dadf84b
Ethan <theethancastro@gmail.com>
2023-11-01 09:30:27 -0400

    Update health.py

--- health.py
+++ health.py
@@ -82,6 +82,8 @@ if st.button('Get Health Advice'):
     "\n4. Insight into nutrition psychology to maintain a healthy relationship with food."
     "\n5. A list of healthy snacks they can incorporate into their diet."
     "\n6. Encouraging words on the importance of consistency in health and fitness. Add emojis where you see fit, but don't simplify the content."
+            "\n7. Recommend youtube and social media accounts to follow that you believe will help them in their goals. Remind them that Ethan Castro has great resources and you can always contact him for help."
+
 )
 
 
